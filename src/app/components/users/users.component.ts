a6902652c3fa3338efc3a5dbbf895bbf4bcf0a69
akanshmahendra <akki619@icloud.com>
2018-09-16 04:11:03 +0530

    2 way databinding

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,10 +9,20 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
 
+  user : User = {
+    firstName : '',
+    lastName : '',
+    age : null,
+    address : {
+      street : '',
+      city : '',
+      state : ''
+    }
+  };
   users : User[];
   showExtended : boolean = true;
   loaded : boolean = false;
-  enableAdd : boolean = true;
+  enableAdd : boolean = false;
   showUserForm : boolean = false;
 
   constructor() { }
@@ -64,9 +74,21 @@ export class UsersComponent implements OnInit {
     this.loaded = true;
   }
 
-  addUser(user : User)
+  addUser()
   {
-    this.users.push(user);
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    this.users.unshift(this.user);
+    this.user = {
+      firstName : '',
+      lastName : '',
+      age : null,
+      address : {
+        street : '',
+        city : '',
+        state : ''
+      }
+    }
   }
 
   // toggleHide(user : User)
