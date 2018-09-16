e8a0c50e1fc5d5bc1683065ca4e6802c69114c99
akanshmahendra <akki619@icloud.com>
2018-09-16 19:42:14 +0530

    Update Post

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { PostService } from '../../services/post.service';
 import { Post } from '../../models/Post';
 
@@ -9,12 +9,16 @@ import { Post } from '../../models/Post';
 })
 export class PostFormComponent implements OnInit
 {
-  post : Post;
   @Output() newPost : EventEmitter<Post> = new EventEmitter();
+  @Output() updatedPost : EventEmitter<Post> = new EventEmitter();
+  @Input() currentPost : Post;
+  @Input() isEdit : boolean;
+
   constructor(private postService : PostService) { }
 
   ngOnInit() {
   }
+
   addPost(title, body)
   {
     if(!title || !body)
@@ -27,5 +31,14 @@ export class PostFormComponent implements OnInit
         this.newPost.emit(post);
       });
     }
+  }
+
+  updatePost()
+  {
+    this.postService.updatePost(this.currentPost).subscribe(post => {
+      console.log(post);
+      this.isEdit = false;
+      this.updatedPost.emit(post);
+    });
   }
 }
